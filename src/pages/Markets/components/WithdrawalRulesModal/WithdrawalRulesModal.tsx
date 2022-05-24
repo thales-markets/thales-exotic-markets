aae5f0deed0a55a48f4698dda1582a1c33affce3
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-05-24 10:55:56 +0200

    Fix positions overlap

---
+++ src/pages/Markets/components/WithdrawalRulesModal/WithdrawalRulesModal.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import { FlexDivColumnCentered } from 'styles/common';
+import Modal from 'components/Modal';
+
+type WithdrawalRulesModalProps = {
+    onClose: () => void;
+    withdrawalPeriodInHours: string | number;
+};
+
+export const WithdrawalRulesModal: React.FC<WithdrawalRulesModalProps> = ({ onClose, withdrawalPeriodInHours }) => {
+    const { t } = useTranslation();
+
+    return (
+        <Modal title={t('market.withdrawal-rules-modal.title')} onClose={onClose}>
+            <Container>
+                <Trans
+                    i18nKey="market.withdrawal-rules-modal.text"
+                    components={[
+                        <ul key="1">
+                            <li key="0" />
+                        </ul>,
+                    ]}
+                    values={{ withdrawalPeriodInHours }}
+                />
+            </Container>
+        </Modal>
+    );
+};
+
+const Container = styled(FlexDivColumnCentered)`
+    color: ${(props) => props.theme.textColor.primary};
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 18px;
+    text-align: justify;
+    ul {
+        list-style: initial;
+        margin-left: 15px;
+    }
+    li {
+        :not(:last-child) {
+            margin-bottom: 10px;
+        }
+    }
+    max-width: 500px;
+`;
+
+export default WithdrawalRulesModal;
