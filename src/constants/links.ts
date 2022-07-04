2d262c48c24aa1f923e6b15801f38bfe714c870d
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-07-04 17:14:27 +0200

    Add footer

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -5,4 +5,12 @@ export const LINKS = {
         'https://github.com/thales-markets/thales-improvement-proposals/blob/main/TIPs/Oracle_Council.md',
     ThalesTip28: 'https://github.com/thales-markets/thales-improvement-proposals/blob/main/TIPs/TIP-28.md',
     ExoticMarkets: 'https://exoticmarkets.xyz/#/',
+    Footer: {
+        Twitter: 'https://twitter.com/ex0t1c_markets',
+        Discord: 'https://discord.gg/thales',
+        Docs: 'https://docs.exoticmarkets.xyz',
+        Medium: 'https://thalesmarket.medium.com/',
+        GitHub: 'https://github.com/thales-markets/thales-exotic-markets',
+        Tutorial: 'https://www.youtube.com/watch?v=Mx4r_R5A4eQ',
+    },
 };
