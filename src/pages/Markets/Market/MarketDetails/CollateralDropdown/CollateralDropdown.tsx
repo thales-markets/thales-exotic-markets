84fb30c9f19addf6c710a14e5e4afa6f8baf8dd6
Gorshtak <sg31191@gmail.com>
2022-09-14 14:13:48 +0200

    changed collaterall dropdown to always visible

--- src/pages/Markets/Market/MarketDetails/CollateralDropdown/CollateralDropdown.tsx
+++ src/pages/Markets/Market/MarketDetails/CollateralDropdown/CollateralDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 // import Dai from 'assets/currencies/DAI.svg';
 // import sUSD from 'assets/currencies/sUSD.svg';
@@ -33,34 +33,28 @@ type CollateralProps = {
 };
 
 const CollateralDropdown: React.FC<CollateralProps> = ({ collateral, setCollateral }) => {
-    const [dropdownOpen, setOpen] = useState(false);
     const networkId = useSelector((state: RootState) => getNetworkId(state));
     const walletAddress = useSelector((state: RootState) => getWalletAddress(state)) || '';
     const useCollateralQuery = useCollateralBalanceQuery(walletAddress, networkId, {
-        enabled: dropdownOpen,
+        enabled: true,
     });
 
     const balances = useCollateralQuery.isSuccess ? useCollateralQuery.data : [];
 
     return (
-        <Wrapper onClick={setOpen.bind(this, !dropdownOpen)}>
-            {!dropdownOpen && <Text>{collateral.name}</Text>}
-            {dropdownOpen && (
-                <CollateraWrapper>
-                    {AVAILABLE_COLLATERALS.map((token, index) => {
-                        return (
-                            <Row key={token.address} onClick={setCollateral.bind(this, token)}>
-                                <FlexDivCentered>
-                                    <Img src={token.logoURI}></Img>
-                                    <Balance style={{ marginLeft: 10 }}>{token.symbol}</Balance>
-                                </FlexDivCentered>
-
+        <Wrapper>
+            <CollateraWrapper>
+                {AVAILABLE_COLLATERALS.map((token, index) => {
+                    return (
+                        <Row key={token.address} onClick={setCollateral.bind(this, token)}>
+                            <FlexDivCentered style={{ opacity: collateral.address === token.address ? 1 : 0.2 }}>
+                                <Img src={token.logoURI}></Img>
                                 <Balance>{balances ? formatCurrencyWithSign('', balances[index], 2) : 0}</Balance>
-                            </Row>
-                        );
-                    })}
-                </CollateraWrapper>
-            )}
+                            </FlexDivCentered>
+                        </Row>
+                    );
+                })}
+            </CollateraWrapper>
         </Wrapper>
     );
 };
@@ -68,61 +62,46 @@ const CollateralDropdown: React.FC<CollateralProps> = ({ collateral, setCollater
 export default CollateralDropdown;
 
 const Wrapper = styled.div`
-    position: absolute;
-    width: 233px;
-    min-height: 28px;
-    left: 10px;
-    top: 10px;
-    cursor: pointer;
-    border: 2px solid #715098;
-    border-radius: 30px;
-    background: white;
+    position: relative;
+    top: -30px;
 `;
 
 const CollateraWrapper = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: space-between;
-    height: 192px;
 `;
 
 const Row = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 0 16px;
-    &:hover {
-        background: rgba(0, 0, 0, 0.05);
-    }
-    &:last-child {
-        border-radius: 0 0 20px 20px;
-    }
-    &:first-child {
-        border-radius: 20px 20px 0 0;
-    }
+    cursor: pointer;
 `;
 
 const Img = styled.img`
     width: 36px;
     height: 36px;
+    margin-right: 10px;
 `;
 
-const Text = styled.p`
-    font-family: 'Nunito';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 17px;
-    line-height: 23px;
-    text-align: center;
-    color: #715098;
-    &:after {
-        font-family: ExoticIcons !important;
-        content: '\\004D';
-        position: absolute;
-        font-size: 21.5px;
-        left: 200px;
-        top: 6px;
-    }
-`;
+// const Text = styled.p`
+//     font-family: 'Nunito';
+//     font-style: normal;
+//     font-weight: 400;
+//     font-size: 17px;
+//     line-height: 23px;
+//     text-align: center;
+//     color: #715098;
+//     &:after {
+//         font-family: ExoticIcons !important;
+//         content: '\\004D';
+//         position: absolute;
+//         font-size: 21.5px;
+//         left: 200px;
+//         top: 6px;
+//     }
+// `;
 
 const Balance = styled.p`
     font-family: 'Nunito';
